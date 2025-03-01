2040812870ba85194f2eafd98fc2b962c8093163
Tuba761 <shabberhighway@gmail.com>
2025-03-01 13:08:06 -0800

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,2 +1,13 @@
-let personName: string= "Eric";
-console.log(`hello ${personName} would you like to learn some python today?`);
+const personName = "tuBa nAz";
+console.log(`Name in Uppercase : ${personName.toUpperCase()}`);
+console.log(`Name in Lowercase : ${personName.toLowerCase()}`);
+
+//for title case
+
+let step1 = personName.split(` `);
+
+let step2 = step1.map(word => word.charAt(0).toUpperCase() + word.substr(1).toLocaleLowerCase());
+
+let step3 = step2.join(` `);
+
+console.log(`Name in title case : ${step3}`);
